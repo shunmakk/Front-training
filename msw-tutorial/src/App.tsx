a614707acf375278329b4f800e0373e507fbaeed
uni <147740687+shunmakk@users.noreply.github.com>
2025-02-24 14:27:29 +0900

    mock再定義

--- msw-tutorial/src/App.tsx
+++ msw-tutorial/src/App.tsx
@@ -1,14 +1,11 @@
 import "./App.css";
 import useSWR from "swr";
-
-type ResponseDate = {
-  text: string;
-};
+import { User } from "./mocks/handler";
 
 function App() {
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-  const { data, isLoading, error } = useSWR<ResponseDate>("/resource", fetcher);
+  const { data, isLoading, error } = useSWR<User[]>("/api/users", fetcher);
 
   if (error) {
     return <p>error</p>;
@@ -18,7 +15,16 @@ function App() {
     return <p>Loading now...</p>;
   }
 
-  return <>取得データ:{data?.text}</>;
+  return (
+    <>
+      取得データ:
+      {data?.map((user) => (
+        <div key={user.id}>
+          ID: {user.id}, Name: {user.name}
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default App;
--- msw-tutorial/src/mocks/handler.ts
+++ msw-tutorial/src/mocks/handler.ts
@@ -1,8 +1,20 @@
 import { http, HttpResponse } from "msw";
 
+export type User = {
+  id: number;
+  name: string;
+};
+
+const userList: User[] = [
+  { id: 1, name: "テストユーザー1" },
+  { id: 2, name: "テストユーザー2" },
+  { id: 3, name: "テストユーザー3" },
+  { id: 4, name: "テストユーザー4" },
+];
+
 //apiモックの定義
 export const handlers = [
-  http.get("/resource", () => {
-    return HttpResponse.json({ text: "ハローワールド" });
+  http.get("/api/users", () => {
+    return HttpResponse.json(userList);
   }),
 ];
