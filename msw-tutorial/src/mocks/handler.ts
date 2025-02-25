baf3714eb3d2fa6bce55c63c2a14117a4266160c
uni <147740687+shunmakk@users.noreply.github.com>
2025-02-25 16:43:26 +0900

    クエリパラメーターの所得

--- msw-tutorial/src/mocks/handler.ts
+++ msw-tutorial/src/mocks/handler.ts
@@ -5,6 +5,8 @@ export type User = {
   name: string;
 };
 
+type CreateUserFormData = Pick<User, "name">;
+
 const userList: User[] = [
   { id: 1, name: "テストユーザー1" },
   { id: 2, name: "テストユーザー2" },
@@ -14,16 +16,41 @@ const userList: User[] = [
 
 //apiモックの定義
 export const handlers = [
-  http.get("/api/users", () => {
-    return HttpResponse.json(userList);
+  // ユーザー一覧取得api-クエリパラメータの取得
+  http.get("/api/users", ({ request }) => {
+    const url = new URL(request.url);
+    const userName = url.searchParams.get("name");
+
+    if (!userName) {
+      return HttpResponse.json(userList);
+    }
+
+    const filteredUserList = userList.filter((user) => {
+      return user.name === userName;
+    });
+
+    return HttpResponse.json(filteredUserList);
   }),
 
-  //serListの中から、パスパラメータのuserIdに紐づくユーザーをレスポンスとして返
+  //特定のuser取得API
   http.get("/api/users/:userId", ({ params }) => {
     const userId = Number(params.userId);
     const user = userList.filter((item) => {
       return item.id === Number(userId);
     })[0];
     return HttpResponse.json(user);
+  }),
+
+  //ユーザー作成API
+  http.post("/api/users", async ({ request }) => {
+    const body = (await request.json()) as CreateUserFormData;
+    const newUser: User = { id: userList.length + 1, name: body.name };
+    userList.push(newUser);
+
+    //空のレスポンス返却
+    return new HttpResponse(null, {
+      status: 201,
+      statusText: "success",
+    });
   }),
 ];
